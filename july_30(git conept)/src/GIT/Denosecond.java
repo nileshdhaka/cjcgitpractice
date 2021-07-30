5ebc328754b2bc931ba37f5d6f53be9e55b7e3fe
nilesh <nilesh@ad>
2021-07-30 15:20:20 +0530

    by nil demo 2

---
+++ july_30(git conept)/src/GIT/Denosecond.java
@@ -0,0 +1,14 @@
+package GIT;
+
+public class Denosecond {
+
+	public static void main(String[] args) {
+		
+		System.out.println("demo second GIT");
+	}
+	
+	public void name() 
+	{
+		System.out.println("GIT succsefully commited ");
+	}
+}
